478eca86c8f345289bccd2a1c46785d3280dab09
unknown <asifmasih9@gmail.com>
2017-01-19 16:04:29 +0545

    Updating

--- app.js
+++ app.js
@@ -27,3 +27,34 @@ if (b === undefined){
 }
 else {console.log ('b is defined');
 }
+
+function b() {
+	var myVar;
+    console.log(myVar);
+}
+
+function a() {
+	var myVar = 2;
+    console.log(myVar);
+	b();
+}
+
+var myVar = 1;
+console.log(myVar);
+// a();
+console.log(myVar);
+
+function b(){
+    var myVar;
+    console.log(myVar);
+}
+
+function a(){
+    var myVar =2;
+    console.log(myVar);
+    b();
+}
+var myVar = 1;
+console.log(myVar);
+// a();
+console.log(myVar);
