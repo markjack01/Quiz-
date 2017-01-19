d2d6be85148ab949607d8372a8e656b981424552
unknown <asifmasih9@gmail.com>
2017-01-19 14:10:35 +0545

    CSS File

---
+++ app.js
@@ -0,0 +1,29 @@
+
+    b();
+    console.log(a);
+    var a = 'Hello World';
+
+
+    function b(){
+console.log('Called b!')
+    }
+
+
+var a; 
+console.log(a);
+
+if (a === undefined){
+    console.log('a is undefined!');
+}
+else {
+    console.log('a is defined!');
+}
+
+var b;
+console.log(a);
+
+if (b === undefined){
+    console.log('b is undefined')
+}
+else {console.log ('b is defined');
+}
